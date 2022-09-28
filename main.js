ace55458ac1cb06de38f25c6e8ce36e1ec7a1608
edison <edisonquu@gmail.com>
2022-09-27 23:17:00 -0400

    THE STATE OF IT RIGHT NOW

--- main.js
+++ main.js
@@ -2,10 +2,32 @@
 
 let myLibrary = []
 
-function book(){
 
+
+
+function Book(title, author, pages, read){
+
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+   this.info = function( ){
+       return (this.title +", "+ this.author+", "+this.pages+", "+ this.read)
+    }
 }
 
-function addBookToLibrary(){
+
+let theHobbit = new Book("The Hobbit","J.R.R. Tolkien", "295 pages","False")
+console.log(theHobbit.info())
+
+let title = document.getElementById("title").value
+console.log(title)
+
+function addBookToLibrary(title, author, pages, read){
+
+
+
+
 
 }
